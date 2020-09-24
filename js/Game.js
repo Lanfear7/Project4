248d36d364a4f163a9dcfd12fd9ec4ed30b4f062
ghost <brennan1086@hotmai.com>
2020-09-24 17:29:51 -0600

    Game class getRandomPhrase() will return expected values. Need to link Phrase.js to Game.js to access methods from both.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,77 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+
+    constructor(missed, phrase, activePhrase){
+        this.missed = missed;
+        this.phrase = phrase;
+        this.activePhrase = activePhrase;
+
+        this.missed  = 0;
+        this.phrase = [
+            {phrase: 'It is what it is'},
+            {phrase: 'Jones Good Ass BBQ and Foot Massage'},
+            {phrase: 'What are those'},
+            {phrase: 'yankee with no brim'},
+            {phrase: 'brim with no yankee'},
+            {phrase: 'now thats alot of damage'}
+        ]
+        this.activePhrase = null;
+
+
+    }
+
+    startGame(){
+        let startBtn = document.querySelector('#btn__reset')
+        let startBackground = document.querySelector('#overlay')
+
+        startBtn.addEventListener('click', (e) => {
+            console.log(e.target)
+            startBackground.style.display = 'none'
+
+            //this will call getRandomPhrase\/
+            let g = this.getRandomPhrase()
+            console.log(g)
+            this.addPhraseToDisplay()
+            
+        })
+
+    }
+
+
+    getRandomPhrase(){
+        //get random number
+        let randomNum = Math.floor(Math.random() * 6);
+        console.log(randomNum);
+        //this will log the value of the phrase object in the phrase array to the console 
+        let randomPhrase = this.phrase[randomNum].phrase;
+        console.log(randomPhrase);
+        this.activePhrase = randomPhrase;
+        return randomPhrase;
+
+    }
+
+
+    handelInteraction(){
+
+
+    }
+
+
+    removeLife(){
+
+
+    }
+
+    checkForWin(){
+
+
+    }
+
+    gameOver(){
+
+
+    }
+
+
+}
+const gameTest = new Game();
+console.log(Game);
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,46 +1,64 @@
+  
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
 
- class Phrase{
-     //constructor to be able to use the phrase
-     constructor(phrase){
-         this.phrase = phrase
+class Phrase{
+    //constructor to be able to use the phrase
+    constructor(phrase){
+        this.phrase = phrase
 
-     }
-     //added methods to this class
-     addPhraseToDisplay(){
+    }
+    //added methods to this class
+    addPhraseToDisplay(){
 
-         //creating the DOM elements 
-         let div = document.createElement('div');
-         div.setAttribute('id', 'phrase')
-         div.setAttribute('class', 'section')
-         let ul = document.createElement('ul');
-         div.appendChild(ul);
+        //creating the DOM elements 
+        let div = document.querySelector('#phrase')
+        let ul = document.querySelector('ul')
 
-
-         //this will change HEllO to hello/ log each letter 
-         console.log(this.phrase.toLowerCase())
-         for(let i = 0; i < this.phrase.length; i++ ){
-             if(this.phrase.charAt(i) !== ' '){
-                 let li = document.createElement('li');
-                 li.setAttribute('class', 'hide letter');
-                 li.textContent = this.phrase.charAt(i)
-                 ul.appendChild(li);
-             }else if(this.phrase.charAt(i) === ' '){
+        let lowerCase = this.phrase.toLowerCase();
+        //this will change HEllO to hello/ log each letter 
+        console.log(lowerCase)
+        for(let i = 0; i < this.phrase.length; i++ ){
+            if(this.phrase[i] !== ' '){
+                let li = document.createElement('li');
+                li.setAttribute('class', 'hide letter');
+                li.textContent = this.phrase.charAt(i)
+                ul.appendChild(li);
+            }else if(this.phrase[i] === ' '){
                 let liSpace = document.createElement('li');
                 liSpace.setAttribute('class', 'space');
                 liSpace.textContent = this.phrase.charAt(i)
-                 ul.appendChild(liSpace)
-             }
-             console.log(this.phrase.charAt(i))
-         }
-         console.log(div);
-         
-     };
-     checkLetter(){}
-     showMatchedNumber(){}
- }
+                ul.appendChild(liSpace)
+            }
+            console.log(this.phrase.charAt(i))
+        }
+        console.log(div);
+        
+    };
+    checkLetter(){
+        let keyboard = document.querySelectorAll('button');
+        let hiddenPhrase = document.querySelectorAll('li.hide letter')
+        //adding event listener to all the buttons 
+        console.log(hiddenPhrase);
+        for (let i = 0; i < hiddenPhrase.length; i++){
+            console.log(hiddenPhrase[i])
+        }
+        for (let i = 0; i < keyboard.length; i++){
+            keyboard[i].addEventListener('change', (e) =>{
+                console.log(e.target);
+                //if the letter matches any letter in our phrase 
+                if(e.target){
+
+                }
+            });
+        }
+        
+
+        
+    }
+    showMatchedNumber(){}
+}
 //test phrase 
- const test = new Phrase('H El lo');
- console.log(test);
+const test = new Phrase('H El lo');
+console.log(test);
